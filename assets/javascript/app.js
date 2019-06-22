6a81445ca4821ed2d268df9273dd82eca59d9c85
Christina <christinachon90@gmail.com>
2019-06-21 17:40:36 -0700

    display question 1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,12 @@
 var timeRemaining;
-var questions = ["this is question 1?", "this is question 2?", "this is question 3?"];
+var questions = ["Which is not a Hogwarts house?", "this is question 2?", "this is question 3?"];
 
 
-var answer1 = ["answer1q1", "answer 2q1", "answer 3q1", "answer4q1"];
-var answer1Array = answer1.length;
+var answer1 = ["Ravenclaw", "Hinkypuff", "Slytherin", "Gryffindor"];
 var answer2 = ["answer1q2", "answer 2q2", "answer 3q2", "answer4q2"];
 var answer3 = ["answer1q3", "answer 2q3", "answer 3q3", "answer4q3"];
-var rightAnswers;
-var wrongAnswers;
+var rightAnswers = 0;
+var wrongAnswers = 0;
 
 
 function newGame(){
@@ -17,18 +16,24 @@ $(".quizarea").hide();
 $(".start").click(function(){
     $(".quizarea").show();
     $(this).hide();
+    questionOne();
 });
-//display Q1 and Answers
-$(".question").text(questions[0]);
-// $(".answer1").text(answer1[0]);
-// $(".answer2").text(answer1[1]);
-// $(".answer3").text(answer1[2]);
-// $(".answer4").text(answer1[3]);
-
 };
 
-for(var i=0; i<answer1.length; i++){
-    $(".answer").text(answer1[0]);
-}
+//display Q1 and Answers
+function questionOne(){
+$(".question").text(questions[0]);
+$(".answer1").text(answer1[0]);
+$(".answer2").text(answer1[1]);
+$(".answer3").text(answer1[2]);
+$(".answer4").text(answer1[3]);
+};
+
+
+
+
+
+
+
 
 newGame()
